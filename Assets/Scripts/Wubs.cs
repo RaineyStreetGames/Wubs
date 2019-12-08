860fa3b0933bf7b5989993ec9c29b2eb20078f18
Jesse Michael <jesse.michael@faithlife.com>
2019-12-08 13:07:55 -0700

    Wubs offset.

--- Assets/Scripts/Wubs.cs
+++ Assets/Scripts/Wubs.cs
@@ -10,9 +10,11 @@ public class Wubs : MonoBehaviour
     public AudioClip wub2;
     public AudioClip wub3;
     public AudioClip wub4;
+    public float targetOffset;
 
     private Rigidbody2D rb;
     private Vector3 target;
+    private Vector3 center;
     private bool flying;
     private List<AudioClip> wubList;
     private AudioSource wubSource;
@@ -23,6 +25,7 @@ public class Wubs : MonoBehaviour
         wubList = new List<AudioClip>() { wub1, wub2, wub3, wub4 };
         wubSource = GetComponent<AudioSource>();
         wubSource.clip = wubList[Random.Range(0, wubList.Count)];
+        center = new Vector3(Screen.width / 2, Screen.height / 2, 0);
     }
 
     void Update()
@@ -30,8 +33,9 @@ public class Wubs : MonoBehaviour
         if (Input.GetMouseButton(0))
         {
             flying = true;
-            var mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            target = (new Vector3(mouse.x, mouse.y, 0));
+            var dir = (Input.mousePosition - center) * targetOffset;
+            target = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x - dir.x, Input.mousePosition.y - dir.y, 0));
+            target = new Vector3(target.x, target.y, 0);
         }
 
         if (Input.GetMouseButtonUp(0))
